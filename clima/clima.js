bd9e9475d915ddf8c00c774bbe82ccb54b5212f8
Guillermo Salas Serey <guispro@gmail.com>
2020-09-09 16:45:41 -0300

    Agregamos .env con la configuracion

--- clima/clima.js
+++ clima/clima.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
 const axios = require('axios');
 
+dotenv.config();
+
 const getClima = async ( lat, lng) =>{
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=ef65fc3dd443ae9cd219b51a463b628e`);
+    const owkeyapp = process.env.APP_KEY_OPENWEATHER;
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${owkeyapp}`);
     return resp.data.main.temp;
 }
 
--- lugar/lugar.js
+++ lugar/lugar.js
@@ -1,15 +1,19 @@
+const dotenv = require('dotenv');
 const axios = require('axios');
+
+dotenv.config();
 
 const getLugarLatLng = async ( dir ) => {
 
     const encodedUrl = encodeURI(dir);
+    const owkeyapp = process.env.APP_KEY_OPENWEATHER;
     
     const instance = axios.create({
         baseURL: 'https://api.openweathermap.org',
         headers:{'Content-Type':'application/json'}
       });
     
-    const resp = await instance.get(`/data/2.5/weather?q=${encodedUrl}&units=metric&appid=ef65fc3dd443ae9cd219b51a463b628e`);
+    const resp = await instance.get(`/data/2.5/weather?q=${encodedUrl}&units=metric&appid=${owkeyapp}`);
     
     if ( resp.isAxiosError ) {
         throw new Error(`No hay resultados para ${direccion}`);
